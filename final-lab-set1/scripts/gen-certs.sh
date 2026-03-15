632486afb04919e293d976487a6e0efc4de633d4
NateHigger <NateHigger@Agartha.com>
2026-03-15 15:32:04 +0700

    tiny ahh tweak

--- final-lab-set1/scripts/gen-certs.sh
+++ final-lab-set1/scripts/gen-certs.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-
-mkdir -p certs
-
-openssl req -x509 -nodes -days 365 \
--newkey rsa:2048 \
--keyout certs/server.key \
--out certs/server.crt \
--subj "/CN=localhost"
-
-echo "Certificates generated"
+# สร้าง self-signed certificate สำหรับ development
+mkdir -p nginx/certs
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+  -keyout nginx/certs/key.pem \
+  -out    nginx/certs/cert.pem \
+  -subj "/C=TH/ST=Bangkok/L=Bangkok/O=RMUTL/OU=ENGCE301/CN=localhost"
+echo "✅ Certificate created in nginx/certs/"
