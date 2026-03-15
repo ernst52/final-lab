82988d204531420fb458116b1b08055c425b4ead
NateHigger <NateHigger@Agartha.com>
2026-03-15 15:20:36 +0700

    add scripts to gen-certs

---
+++ final-lab-set1/scripts/gen-certs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p certs
+
+openssl req -x509 -nodes -days 365 \
+-newkey rsa:2048 \
+-keyout certs/server.key \
+-out certs/server.crt \
+-subj "/CN=localhost"
+
+echo "Certificates generated"
